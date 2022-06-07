d8b5c56bc28a007069a14350ce540b8495f2cba8
Lisa <lisa.hl.chen@gmail.com>
2022-06-07 14:27:18 -0700

    basic jQuery function

---
+++ jQueryFunctions.js
@@ -0,0 +1,27 @@
+// .click
+// .hide
+// .show
+// .toggle
+// .slideDown
+// .slideUp
+// .slideToggle
+// .fadeIn
+// .fadeOut
+// .addClass
+// .before
+// .after
+// .append
+// .html
+// .attr
+// .val
+// .text
+$('button.addClass').click(function(){
+    $('textAddClass').addClass("redClass");
+})
+
+    // $('button').click(function(){
+    // $('#myParagraph').text('see how I .text() works as a setter!');
+$('.hide').click(function(){
+    $('#odd').hide();
+})
+
